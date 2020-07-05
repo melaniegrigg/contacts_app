e058ce2372e1ff1cc5ca0128d46f730c174eb7b5
melaniegrigg <melanie.grigger@gmail.com>
2020-07-05 13:41:49 -0400

    add create restful route

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -8,4 +8,15 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find_by(id: params[:id])
     render 'show.json.jb'
   end
+
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    @contact.save
+    render 'show.json.jb'
+  end
 end
